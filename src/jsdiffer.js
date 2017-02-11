120ed77e77973a071310e4e1521b6003b3fd0a87
Jun Zheng <junzheng@exchange.niyume.com>
2017-02-11 17:48:53 -0500

    Added more APIs, diff(), _lcs_length(), _lcs_diff()

--- src/jsdiffer.js
+++ src/jsdiffer.js
@@ -1,6 +1,7 @@
 class JsDiffer {
     constructor(){
         this._lcs_sol = {};   
+        this._diff_res = [];
     }
 
     setX(new_x) {
@@ -13,6 +14,51 @@ class JsDiffer {
 
     lcs(){
         return this._lcs_recursive(this._x, this._y, this._x.length - 1, this._y.length - 1);
+    }
+
+    _lcs_length(x, y) {
+        var c = []
+        for (var i = 0; i <= x.length; i++){
+            c[i] = [];    
+            for (var j = 0; j <= y.length; j++){
+                c[i][j] = 0;
+            }
+        }
+        for (var i = 1; i <= x.length; i++){
+            for (var j = 1; j <= y.length; j++){
+                if (x[i] == y[j]){
+                    c[i][j] = c[i-1][j-1] + 1
+                } else {
+                    c[i][j] = Math.max(c[i][j-1],c[i-1][j])
+                }
+            }
+        }
+        console.log(JSON.stringify(c))
+        return c;
+    }
+
+    _lcs_diff(c, x, y, i, j){
+        if (i >= 0 && j >= 0 && x[i] == y[j]){
+            this._lcs_diff(c, x, y, i-1, j-1);
+            this._diff_res.push(["#", [i,j] , x[i]]);
+            return [x[i]];
+        } else if (j > 0 && (i== 0 || c[i][j-1] >= c[i-1][j])){
+            this._lcs_diff(c, x, y, i, j-1);
+            this._diff_res.push(["+", [i,j] , y[j]]);
+            return ["+ " + y[j]]
+        } else if (i > 0 && (j == 0 || c[i][j-1] < c[i-1][j])){
+            this._lcs_diff(c, x, y, i-1, j);
+            this._diff_res.push(["-", [i,j] , x[i]]);
+            return ["- " + x[i]]
+        } else {
+            return []
+        }
+    }
+
+    diff(){
+        this._diff_res = [];
+        this._lcs_diff(this._lcs_length(this._x, this._y), this._x, this._y, this._x.length - 1, this._y.length - 1);
+        return this._diff_res;
     }
 
     _lcs_recursive(x,y,i,j) {
@@ -31,13 +77,14 @@ class JsDiffer {
             } else {
                 var sol_a = this._lcs_recursive(x,y,i-1,j);
                 var sol_b = this._lcs_recursive(x,y,i,j-1);
-                if (sol_a.length > sol_b.length){
-                    sol = sol_a;
-                } else {
-                    sol = sol_b;
-                }
+                if (i > 0 && (j == 0 || sol_a.length > sol_b.length)){
+                    sol = sol.concat(["- " + x[i]]);
+                } else if (j > 0 && (i == 0 || sol_a.length <= sol_b.length)) {
+                    sol = sol.concat(["+ " + y[j]]);
+                }    
             }
-            this._lcs_push_sol(x,y,i,j,sol);
+            //this._lcs_push_sol(x,y,i,j,sol);
+            console.log(sol);
             return sol;
         }
     }
